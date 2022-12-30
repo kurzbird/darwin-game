59e73c13568d760febd5932ab5cbc97235d79545
Jeff <herrbourne@gmail.com>
2022-12-30 00:44:28 -0800

    Add item fade in animations for MutationDisplay

--- src/components/MutationDisplay.js
+++ src/components/MutationDisplay.js
@@ -17,6 +17,26 @@ function MutationDisplay({ genePool, onSelectMutation, text, refreshGenePool, cl
         setMutationsToDisplay(refreshGenePool)
     }
 
+    const containerVariants = {
+        hidden: { opacity: 1, scale: 0 },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            transition: {
+                delayChildren: 0.3,
+                staggerChildren: 0.2
+            }
+        }
+    }
+
+    const itemVariants = {
+        hidden: { y: 20, opacity: 0 },
+        visible: {
+            y: 0,
+            opacity: 1
+        }
+    }
+
     return (
         <AnimatePresence>
             <motion.div
@@ -26,17 +46,25 @@ function MutationDisplay({ genePool, onSelectMutation, text, refreshGenePool, cl
                 className="popup"
             >
                 <div className="mutation-display">
-                    <div className="mutation-display-inner">
+                    <motion.div
+                        variants={containerVariants}
+                        initial="hidden"
+                        animate="visible"
+                        className="mutation-display-inner"
+                    >
                         <h4 className="display-header">{text} <br /><br /></h4>
                         <div className="random-mutations">
-                            {mutationsToDisplay.map((Mutation, i) => <p onClick={() => selectMutation(Mutation)} key={i}>Mutation {i + 1}: {Mutation.legs}, {Mutation.size}, {Mutation.horns}, {Mutation.hair}, {Mutation.camo}</p>)}
+                            {mutationsToDisplay.map((Mutation, i) =>
+                                <motion.p onClick={() => selectMutation(Mutation)} key={i} variants={itemVariants}>
+                                    Mutation {i + 1}: {Mutation.legs}, {Mutation.size}, {Mutation.horns}, {Mutation.hair}, {Mutation.camo}
+                                </motion.p>)}
                         </div>
 
                         <Button text="More Mutations" onClick={moreMutations} />
                         {!starterDisplay && <Button text="Close" onClick={closeDisplay} />}
-                    </div>
+                    </motion.div>
                 </div>
-            </motion.div>
+            </motion.div >
         </AnimatePresence >
     )
 }
