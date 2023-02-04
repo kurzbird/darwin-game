b431912f2e812f47fda5ac2ff4e896a9c7549d64
Jeff <herrbourne@gmail.com>
2023-02-03 23:38:33 -0800

    Set up PostGame data

--- src/components/PostGame.js
+++ src/components/PostGame.js
@@ -1,38 +1,39 @@
 import React from "react";
 import './PostGame.css';
 
-const PostGame = () => {
+const PostGame = ( { mostCommonYetis, roundPopulations, roundCatastrophes }) => {
     return (
         <div className="postgame-container">
             <div className="round-recap">
                 <div className="year-1">
-                    <div>Most Common Yeti: </div>
-                    <div>Population Size: </div>
-                    <div>Environmental Event: </div>
+                    {/* TO-DO: Add image of most common yeti with stats */}
+                    <div>Most Common Yeti: {mostCommonYetis[0]}</div>
+                    <div>Population Size: {roundPopulations[0]}</div>
+                    <div>Environmental Event: {roundCatastrophes[0]}</div>
                 </div>
 
                 <div className="year-124860">
-                    <div>Most Common Yeti: </div>
-                    <div>Population Size: </div>
-                    <div>Environmental Event: </div>
+                    <div>Most Common Yeti: {mostCommonYetis[1]} </div>
+                    <div>Population Size: {roundPopulations[1]} </div>
+                    <div>Environmental Event: {roundCatastrophes[1]}</div>
                 </div>
 
                 <div className="year-419868">
-                    <div>Most Common Yeti: </div>
-                    <div>Population Size: </div>
-                    <div>Environmental Event: </div>
+                    <div>Most Common Yeti: {mostCommonYetis[2]}</div>
+                    <div>Population Size: {roundPopulations[2]} </div>
+                    <div>Environmental Event: {roundCatastrophes[2]}</div>
                 </div>
 
                 <div className="year-692853">
-                    <div>Most Common Yeti: </div>
-                    <div>Population Size: </div>
-                    <div>Environmental Event: </div>
+                    <div>Most Common Yeti: {mostCommonYetis[3]}</div>
+                    <div>Population Size: {roundPopulations[3]}</div>
+                    <div>Environmental Event: {roundCatastrophes[3]}</div>
                 </div>
 
                 <div className="year-1000000">
-                    <div>Most Common Yeti: </div>
-                    <div>Population Size: </div>
-                    <div>Environmental Event: </div>
+                    <div>Most Common Yeti: {mostCommonYetis[4]}</div>
+                    <div>Population Size: {roundPopulations[4]}</div>
+                    <div>Environmental Event: {roundCatastrophes[4]}</div>
                 </div>
             </div>
         </div>
